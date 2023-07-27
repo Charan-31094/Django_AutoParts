9c5e2af436ca6929303fa5601d93445331abd9ea
Charan <109283050+Charan-31094@users.noreply.github.com>
2023-07-27 12:16:06 +0530

    Add files via upload

---
+++ pr2/app1/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Registration, Feedback,product,categories,cart,payments,orders
+
+admin.site.register(Registration),
+admin.site.register(Feedback),
+admin.site.register(product),
+admin.site.register(categories),
+admin.site.register(cart),
+admin.site.register(payments),
+admin.site.register(orders)
